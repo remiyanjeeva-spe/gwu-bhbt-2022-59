a94c88b911f2eceb8340a25303584f5beea67d56
remi20011 <jeevaremiyan@gmail.com>
2025-12-31 14:26:50 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/gwu-bhbt-2022-59/",
+  base: "/gwu-bhbt-2022-59profile/",
   plugins: [react()]
 });
